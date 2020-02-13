20dae8efe3de3e34b3c4c7c0e64b2c3d167309f5
Aidan <aidoraide@gmail.com>
2020-02-13 13:58:09 -0800

    added ability to solve for i

--- calculator.py
+++ calculator.py
@@ -27,40 +27,80 @@ pattern2formula = {
     r'P/A': 'PA',
 }
 
-def sub(match):
-    match_args = match.group().strip('(').strip(')').split(',')
-    pattern, i, N = match_args
-    func, i, N = pattern2formula[pattern], float(i), int(N)
-    code = f'{func}({i}, {N})'
-    return code
+def sub(allow_symbols):
+    def f(match):
+        match_args = match.group().strip('(').strip(')').split(',')
+        pattern, i, N = match_args
+        func, N = pattern2formula[pattern], int(N)
+        if not allow_symbols:
+            i = float(i)
+        code = f'{func}({i}, {N})'
+        return code
+    return f
 
 def sub_assume_multiplication(match):
     group = match.group()
     return group.replace('(', '*(').replace(')', ')*')
 
-def parse_user_input(user_in):
+def parse_user_input(user_in, allow_symbols):
     evaluation = user_in
     evaluation = evaluation.replace(' ', '')   # no whitespace
     evaluation = evaluation.replace('^', '**') # regular exponents to Python exponents
     evaluation = re.sub(r'[0-9]+\(|\)[0-9]+', sub_assume_multiplication, evaluation) # 500(F/A, ...) -> 500 * (F/A, ...)
     for pattern, formula_func in pattern2formula.items():
         # (formula, float, int)
-        match_regex = f'\({pattern},[0-9]*\.?[0-9]*,[0-9]+\)'
-        evaluation = re.sub(match_regex, sub, evaluation)
+        match_regex = f'\({pattern},[0-9i]*\.?[0-9]*,[0-9]+\)'
+        evaluation = re.sub(match_regex, sub(allow_symbols), evaluation)
     return evaluation
+
+
+# Find where func == 0
+def solve(func, lo=0, hi=10000):
+    # Swap the sign of function if not increasing
+    if func(0.7) - func(0.6) < 0:
+        return solve(lambda x: -func(x), lo, hi)
+    
+    # func is constantly increasing
+    mid = (lo + hi) / 2
+    fmid = func(mid)
+    if abs(fmid) < 1e-8:
+        return mid
+    elif fmid < 0:
+        return solve(func, mid, hi)
+    else:
+        return solve(func, lo, mid)
+
+
+def evaluate_for_i(evaluation):
+    def func(i):
+        # Sets the value of i before eval
+        return eval(evaluation)
+    return func
+
 
 def handle_simple_evaluation(user_in):
     try:
-        evaluation = parse_user_input(user_in)
-        value = eval(evaluation)
-        print(value)
-        print(user_in, '=', round(value, 2))
+        if '=' in user_in:
+            l, r = user_in.split('=')
+            levaluation = parse_user_input(l, True)
+            revaluation = parse_user_input(r, True)
+            i = solve(evaluate_for_i(revaluation + '-' + levaluation))
+            print(f'i = {i}')
+            print(f'{user_in}, for i = {round(i, 2)}')
+        else:
+            evaluation = parse_user_input(user_in, False)
+            value = eval(evaluation)
+            print(value)
+            print(user_in, '=', round(value, 2))
     except KeyboardInterrupt as e:
         exit()
     except SyntaxError as e:
         print(f'Error when evaluating "{evaluation}" as Python code')
     except NameError as e:
         print(f'Error when evaluating "{evaluation}" as Python code')
+    except ValueError as e:
+        print(f'Error when evaluating expression. An input of i was given for interest rate but the input was not an equation')
+        print(e)
 
 
 user_in = ''
